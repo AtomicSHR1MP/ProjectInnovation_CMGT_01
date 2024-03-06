e2de228c34d559d56a80e731f4dd1506fc6370e5
Tim Sommer <tim.sommer.home@hotmail.de>
2024-03-06 14:38:51 +0100

    New Tiles and sounds

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,33 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class Coin : MonoBehaviour {
 
     [SerializeField] float turnSpeed = 90f;
+    public AudioClip clip;
+    public AudioSource source;
+
+
+    private void Start()
+    {
+        source.clip = clip;
+
+    }
 
     private void OnTriggerEnter (Collider other)
     {
-        if (other.gameObject.GetComponent<Obstacle>() != null) {
-            Destroy(gameObject);
-            return;
-        }
-
+        /*  if (other.gameObject.GetComponent<Obstacle>() != null) {
+              Destroy(gameObject);
+              return;
+          }
+        */
+        Console.WriteLine("coin collides with " + other.gameObject.name);
         // Check that the object we collided with is the player
-        if (other.gameObject.name != "Player") {
+         if (other.gameObject.name == "Player") {
+            Console.WriteLine("coin plays sound " + other.gameObject.name);
+            // Add to the player's score
+            GameManager.inst.IncrementScore();
+
+            source.clip = clip;
+
+            source.Play();
+
+            MeshRenderer render = gameObject.GetComponentInChildren<MeshRenderer>();
+
+            render.enabled = false;
+
+
+            // Destroy this coin object
+            //  gameObject.SetActive(false);
+            // Destroy(gameObject);
             return;
         }
-
-        // Add to the player's score
-        GameManager.inst.IncrementScore();
-
-        // Destroy this coin object
-        Destroy(gameObject);
+       
+        
+      
     }
 
-    private void Start () {
-
-	}
+ 
 
 	private void Update () {
         transform.Rotate(0, 0, turnSpeed * Time.deltaTime);
