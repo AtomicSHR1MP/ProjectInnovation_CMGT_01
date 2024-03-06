b5493b07fac2e281a5e126b36bb70f64a10a2f77
Tim Sommer <tim.sommer.home@hotmail.de>
2024-03-06 14:38:51 +0100

    New Tiles and sounds

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,46 +1,57 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
 
-public class Coin : MonoBehaviour
-{
+public class Coin : MonoBehaviour {
+
     [SerializeField] float turnSpeed = 90f;
     public AudioClip clip;
     public AudioSource source;
 
+
     private void Start()
     {
         source.clip = clip;
+
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter (Collider other)
     {
-        Debug.Log("Coin collides with " + other.gameObject.name);
-
+        /*  if (other.gameObject.GetComponent<Obstacle>() != null) {
+              Destroy(gameObject);
+              return;
+          }
+        */
+        Console.WriteLine("coin collides with " + other.gameObject.name);
         // Check that the object we collided with is the player
-        if (other.gameObject.CompareTag("Player"))
-        {
-            CollectCoin();
+         if (other.gameObject.name == "Player") {
+            Console.WriteLine("coin plays sound " + other.gameObject.name);
+            // Add to the player's score
+            GameManager.inst.IncrementScore();
+
+            source.clip = clip;
+
+            source.Play();
+
+            MeshRenderer render = gameObject.GetComponentInChildren<MeshRenderer>();
+
+            render.enabled = false;
+
+
+            // Destroy this coin object
+            //  gameObject.SetActive(false);
+            // Destroy(gameObject);
+            return;
         }
+       
+        
+      
     }
 
-    void CollectCoin()
-    {
-        Debug.Log("Coin collected!");
+ 
 
-        GameManager.inst.CollectCoin(); // it will nform GameManager that a coin has been collected
-
-        source.clip = clip;
-        source.Play();
-
-        MeshRenderer render = gameObject.GetComponentInChildren<MeshRenderer>();
-        render.enabled = false;
-
-        // Destroy this coin object
-        // gameObject.SetActive(false);
-        // Destroy(gameObject);
-    }
-
-    private void Update()
-    {
+    private void Update () {
         transform.Rotate(0, 0, turnSpeed * Time.deltaTime);
     }
 }
