e3a759c8abe3386b1d4af61c923849896711bb56
Seventyy <seventyyyyy@gmail.com>
2024-03-07 17:06:51 +0100

    shark audio layering fixed

--- Assets/Scripts/OnCloseAnim.cs
+++ Assets/Scripts/OnCloseAnim.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayAnimOnClose : MonoBehaviour
@@ -11,18 +12,33 @@ public class PlayAnimOnClose : MonoBehaviour
 
     PlayerMovement playerMovement;
     bool animationPlayed = false;
+    static bool isAudioPlaying = false;
     // Start is called before the first frame update
     void Start()
     {
         jawsSource.clip = jaws;
         playerMovement = FindObjectOfType<PlayerMovement>();
         GetComponent<Animator>().enabled = false;
-        jawsSource.Play();
+        
+        
+
     }
+
+    void ChangeVariable()
+    {
+        isAudioPlaying = false;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        if (!isAudioPlaying) {
+            jawsSource.Play();
+            isAudioPlaying = true;
+            Invoke("ChangeVariable", 13f);
+        }
+        
         if ((!animationPlayed) && Math.Abs(playerMovement.transform.position.z - transform.position.z) < 110){
             animationPlayed = true;
            
